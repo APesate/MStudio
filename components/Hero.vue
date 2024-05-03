41d5aef30c15a50b79958b2d5fdb5362353d687d
Pesate Temprano, Andrés (SPLXQ) - KLM <Andres.PesateTemprano@klm.com>
2024-05-03 13:58:41 +0200

    Initial commit

---
+++ components/Hero.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+defineProps({
+  videoSource: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <video autoplay muted loop playsinline>
+    <source :src="videoSource" type="video/mp4" />
+  </video>
+</template>
+
+<style>
+video {
+  position: absolute;
+  object-fit: cover;
+  height: 100vh;
+  min-width: 100%;
+  background: hsl(0, 0%, 0%);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <Hero
+    videoSource="https://videos.pexels.com/video-files/9712579/9712579-uhd_3840_2160_30fps.mp4"
+  />
+</template>
+
+<style></style>
